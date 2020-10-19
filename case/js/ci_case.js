2ec76e7b28ba2fde7b33b0ac13abe98d835dad21
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2020-10-19 13:17:08 -0400

    redirects

--- case/js/ci_case.js
+++ case/js/ci_case.js
@@ -37,7 +37,9 @@ new Vue({
   },
 
   created: function created() {
-    this.memberslug=window.location.search.split('?:')[1];
+    // this.memberslug=window.location.search.split('?:')[1];
+    this.blogslug=window.location.href.split('/');
+    this.blogslug = this.blogslug[this.blogslug.length - 1];
     console.log(this.memberslug);
     this.fetchIndex();
   },
