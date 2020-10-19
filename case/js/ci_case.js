41671a85fd41decce14e481b75e3c5f1d6952b42
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2020-10-19 13:26:15 -0400

    slug-format

--- case/js/ci_case.js
+++ case/js/ci_case.js
@@ -37,9 +37,7 @@ new Vue({
   },
 
   created: function created() {
-    // this.memberslug=window.location.search.split('?:')[1];
-    this.blogslug=window.location.href.split('/');
-    this.blogslug = this.blogslug[this.blogslug.length - 1];
+    this.memberslug=window.location.search.split('?:')[1];
     console.log(this.memberslug);
     this.fetchIndex();
   },
